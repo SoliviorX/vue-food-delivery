20305ca0554b7f89cc5e0022c568e552dc13b23c
shideshan <307042873@qq.com>
2019-03-19 19:36:54 +0800

    1

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,11 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    //函数 { 左侧可以不加空格
+    'space-before-function-paren': 0,
+    //不检测缩进
+    'indent': 0
   },
   parserOptions: {
     parser: 'babel-eslint'
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
+import './cube-ui'
 import App from './App.vue'
+import './register'
+
+import 'common/stylus/index.styl'
 
 Vue.config.productionTip = false
 
