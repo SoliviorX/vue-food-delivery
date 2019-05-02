e25bf23d6e6f1b39b733f20c61342c10842dc586
shideshanxx <307042873@qq.com>
2019-05-02 15:37:52 +0800

    完善

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
-
+const webpack = require('webpack')
 const path = require('path')
 const appData = require('./data.json')
 const seller = appData.seller
 const goods = appData.goods
 const ratings = appData.ratings
 
-//定义resolve函数，__dirname表示项目目录，dir表示需要拼接的目录
+// 定义resolve函数，__dirname表示项目目录，dir表示需要拼接的目录
 function resolve(dir) {
   return path.join(__dirname, dir)
 }
@@ -49,11 +49,16 @@ module.exports = {
       })
     }
   },
+  // 执行命令vue inspect >> output.js ，则可以在自命名的output.js中看到一些webpack配置
+  // 在github中搜索webpack-chain查看该项怎么配置
   chainWebpack(config) {
-    //定义路径
+    // 定义路径
     config.resolve.alias
       .set('components', resolve('src/components'))
       .set('common', resolve('src/common'))
       .set('api', resolve('src/api'))
+    // 插件配置
+    config.plugin('context')
+      .use(webpack.ContextReplacementPlugin, [/moment[/\\]locale$/, /zh-cn/])
   }
 }
